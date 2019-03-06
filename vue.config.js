1d06ac6b9d544e0adf56ffa581705d799a311214
dev7 <846593490@qq.com>
2019-03-06 10:58:44 +0800

    关掉生产环境的sourcemap，修改生产环境资源路径

---
+++ vue.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // 这里是配置上线读取当前目录，默认是根路径，如 /js, /css 等，具体根据项目来
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false,
+}
